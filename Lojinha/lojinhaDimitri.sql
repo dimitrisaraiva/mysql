627ea7343b746037a39af906a68c7f9530c53068
dimitrisaraiva <87093466+dimitrisaraiva@users.noreply.github.com>
2022-05-18 16:36:26 -0300

    Add files via upload

---
+++ Lojinha/lojinhaDimitri.sql
@@ -0,0 +1,31 @@
+/**
+	Lojinha
+    @author Dimitri Saraiva
+*/
+
+show databases;
+
+
+create database lojinhaDimitri;
+
+drop database produto;
+
+use lojinhaDimitri;
+
+
+
+create table produto (
+	codigo int primary key auto_increment,
+    nome varchar(255) not null,
+    estoque varchar(255) not null,
+    valor varchar(255) not null
+);
+
+
+show tables;
+
+
+describe produto;
+
+alter table produto add column descricao varchar(255);
+
